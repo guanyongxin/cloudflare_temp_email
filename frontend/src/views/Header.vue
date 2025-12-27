8f41092288e1648c1f8516d258f5bf802c38c6fd
guanyongxin <52368767+guanyongxin@users.noreply.github.com>
2025-12-27 19:47:13 +0800

    Update title in Header.vue for localization

--- frontend/src/views/Header.vue
+++ frontend/src/views/Header.vue
@@ -51,7 +51,7 @@ const changeLocale = async (lang) => {
 const { locale, t } = useI18n({
     messages: {
         en: {
-            title: 'Cloudflare Temp Email',
+            title: '10min Temp Email',
             dark: 'Dark',
             light: 'Light',
             accessHeader: 'Access Password',
@@ -62,7 +62,7 @@ const { locale, t } = useI18n({
             ok: 'OK',
         },
         zh: {
-            title: 'Cloudflare 临时邮件',
+            title: '10分钟临时邮件',
             dark: '暗色',
             light: '亮色',
             accessHeader: '访问密码',
